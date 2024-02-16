0995627f21fb7914a415df8525a1952371a95754
Suraj Dighe <144806742+SD1435@users.noreply.github.com>
2024-02-16 17:16:51 +0530

    New update

--- Main.java
+++ Main.java
@@ -7,8 +7,9 @@ public class Main {
         System.out.println("This is java program");
         System.out.println("output from java program");
         System.out.println(new Date());
-        System.out.println("hello");
+        System.out.println("hello team");
         System.out.println("Added one more file");
+        System.out.println("Added two more file");
         Student student = new Student();
         student.sayHello();
     }
